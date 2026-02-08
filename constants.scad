jimfunk/goews-rebuilt-openscad

commit afd11abaf7896a347d2c86d26f234a377d8bf7f7

Adjusted thicker cleats hanger offset to 0.65mm

constants.scad
@@ -22,7 +22,7 @@ hanger_bolt_notch_center_height = 16;
 hanger_bolt_notch_head_clearance_diameter = 20;
 
 // Hanger plate offset (thicker cleats only)
-hanger_offset = 1;
+hanger_offset = 0.65;
 
 // Plate width
 plate_width = 41.5;

hanger.scad
@@ -30,10 +30,10 @@ module hanger(variant=variant_original, hanger_tolerance, extended_bottom=false)
         }
 
         // Rear chamfer
-        translate([0, hanger_thickness, 0])
+        translate([-0.1, hanger_thickness, 0])
             rotate([135, 0, 0])
                 linear_extrude(hanger_thickness)
-                    square([hanger_width, hanger_thickness * 2]);
+                    square([hanger_width + 0.2, hanger_thickness * 2]);
 
         // Rear tile cutoff (only on thicker cleats)
         if (variant == variant_thicker_cleats) {
@@ -52,9 +52,9 @@ module hanger(variant=variant_original, hanger_tolerance, extended_bottom=false)
         }
 
         // Cutout
-        translate([hanger_width / 2, 0, hanger_bolt_notch_center_height])
+        translate([hanger_width / 2, -0.1, hanger_bolt_notch_center_height])
             rotate([270, 0, 0])
-                linear_extrude(height = hanger_total_thickness)
+                linear_extrude(height = hanger_total_thickness + 0.2)
                     circle(r=hanger_bolt_notch_radius);
     }
 }
@@ -153,12 +153,12 @@ module hanger_plate(
 
         if (outer_radius) {
             // Outer roundovers
-            translate([0, hanger_total_thickness + plate_thickness, total_plate_height])
+            translate([0, hanger_total_thickness + plate_thickness + 0.1, total_plate_height])
                 rotate([90, 90, 0])
-                    rounding_edge_mask(l=plate_thickness, r=outer_radius, anchor=BOTTOM);
-            translate([total_plate_width, hanger_total_thickness + plate_thickness, total_plate_height])
+                    rounding_edge_mask(l=plate_thickness + 0.2, r=outer_radius, anchor=BOTTOM);
+            translate([total_plate_width, hanger_total_thickness + plate_thickness + 0.1, total_plate_height])
                 rotate([90, 180, 0])
-                    rounding_edge_mask(l=plate_thickness, r=outer_radius, anchor=BOTTOM);
+                    rounding_edge_mask(l=plate_thickness + 0.2, r=outer_radius, anchor=BOTTOM);
         }
     }
 }

server/parts/bin.py
@@ -20,7 +20,7 @@ class BinDefinition(BaseModel):
     lip_thickness: Annotated[float, Field(gte=0)] = 1
     inner_rounding: Annotated[float, Field(gte=0)] = 1
     outer_rounding: Annotated[float, Field(gte=0)] = 0.5
-    hanger_tolerance: Annotated[float, Field(gt=0)] = 0.15
+    hanger_tolerance: Annotated[float, Field(ge=0)] = 0.15
     variant: Variant = Variant.Original
 
 

server/parts/cableclip.py
@@ -27,7 +27,7 @@ class CableclipDefinition(BaseModel):
     gap: Annotated[float, Field(gt=0)] = 10
     lip_thickness: float = 2
     rounding: float = 0.5
-    hanger_tolerance: Annotated[float, Field(gt=0)] = 0.15
+    hanger_tolerance: Annotated[float, Field(ge=0)] = 0.15
     variant: Variant = Variant.Original
 
 

server/parts/cup.py
@@ -18,7 +18,7 @@ class CupDefinition(BaseModel):
     bottom_thickness: Annotated[float, Field(gte=0)] = 2
     inner_rounding: Annotated[float, Field(gte=0)] = 0.5
     outer_rounding: Annotated[float, Field(gte=0)] = 0.5
-    hanger_tolerance: Annotated[float, Field(gt=0)] = 0.15
+    hanger_tolerance: Annotated[float, Field(ge=0)] = 0.15
     variant: Variant = Variant.Original
 
 

server/parts/hook.py
@@ -21,7 +21,7 @@ class HookDefinition(BaseModel):
     post_thickness: float = 6
     lip_thickness: float = 0
     rounding: float = 0.5
-    hanger_tolerance: Annotated[float, Field(gt=0)] = 0.15
+    hanger_tolerance: Annotated[float, Field(ge=0)] = 0.15
     variant: Variant = Variant.Original
 
 

server/parts/mount.py
@@ -44,7 +44,7 @@ class MountDefinition(BaseModel):
     minimum_width: Annotated[float, Field(gte=0)] = 0
     minimum_height: Annotated[float, Field(gte=0)] = 0
     bolt_notch: bool = True
-    hanger_tolerance: Annotated[float, Field(gt=0)] = 0.15
+    hanger_tolerance: Annotated[float, Field(ge=0)] = 0.15
     variant: Variant = Variant.Original
 
 

server/parts/rack.py
@@ -21,7 +21,7 @@ class RackDefinition(BaseModel):
     lip_height: float = 8
     lip_thickness: float = 4
     rounding: float = 0.5
-    hanger_tolerance: Annotated[float, Field(gt=0)] = 0.15
+    hanger_tolerance: Annotated[float, Field(ge=0)] = 0.15
     variant: Variant = Variant.Original
 
 

commit 674d98ddd0316e75494fa115937b99fac31cac7c

Fix inconsistent plate face introduced in afd11ab

constants.scad
@@ -24,6 +24,9 @@ hanger_bolt_notch_head_clearance_diameter = 20;
 // Hanger plate offset (thicker cleats only)
 hanger_offset = 0.75;
 
+// Cleat offset (thicker cleats only)
+cleat_offset = 1;
+
 // Nominal plate width
 plate_width = 42;
 

tile.scad
@@ -10,7 +10,7 @@ module tile_cleat(variant=variant_original) {
         translate([0, 0, tile_hanger_cleat_width])
             union() {
                 if (variant == variant_thicker_cleats) {
-                    linear_extrude(height = hanger_offset)
+                    linear_extrude(height = cleat_offset)
                         square([tile_hanger_cleat_width, tile_hanger_cleat_lower_height + tile_hanger_cleat_width]);
                 }
                 rotate([270, 0, 0])
@@ -23,7 +23,7 @@ module tile_cleat(variant=variant_original) {
                             ]
                         );
             }
-        linear_extrude(height = tile_hanger_cleat_width + hanger_offset)
+        linear_extrude(height = tile_hanger_cleat_width + cleat_offset)
             polygon(
                 points = [
                     [0, tile_hanger_cleat_lower_height + tile_hanger_cleat_width],
@@ -71,7 +71,7 @@ module hex_tile_single(
     fill_right=false
 ) {
     rear_cleat_thickness = variant == variant_original ? tile_hanger_rear_cleat_thickness : 0;
-    cleat_additional_thickness = variant == variant_thicker_cleats ? hanger_offset : 0;
+    cleat_additional_thickness = variant == variant_thicker_cleats ? cleat_offset : 0;
     hanger_x_offset = (tile_width - tile_hanger_width) / 2;
     tile_hanger_y_offset = tile_triangle_height;
     threaded_hole_center_x = tile_width / 2;
@@ -571,7 +571,7 @@ module grid_tile_single(
     mounting_hole_countersink_depth=2
 ) {
     rear_cleat_thickness = variant == variant_original ? tile_hanger_rear_cleat_thickness : 0;
-    cleat_additional_thickness = variant == variant_thicker_cleats ? hanger_offset : 0;
+    cleat_additional_thickness = variant == variant_thicker_cleats ? cleat_offset : 0;
     hanger_x_offset = (tile_width - tile_hanger_width) / 2;
     threaded_hole_center_x = tile_width / 2;
     threaded_hole_center_y = grid_hanger_y_offset + tile_hanger_height + tile_threaded_hole_y_offset_from_hanger_top;